66ce75a554eb1a3270a3b24a42773efa15a97023
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-07-09 11:28:33 -0700

    Added stages controller with CRUD routes

--- models/stage.js
+++ models/stage.js
@@ -27,7 +27,7 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'Stage',
     tableName: 'stages',
-    timestamps: 'false'
+    timestamps: false
   });
   return Stage;
 };
