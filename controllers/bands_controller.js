96a81b81b3ce7a55d776da051c92e15aebb3d7bf
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-07-09 11:08:25 -0700

    Added events controller and CRUD routes

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -2,11 +2,17 @@
 const bands = require('express').Router()
 const db = require('../models')
 const { Band } = db 
+const { Op } = require('sequelize')
 
 // FIND ALL BANDS
 bands.get('/', async (req, res) => {
     try {
-        const foundBands = await Band.findAll()
+        const foundBands = await Band.findAll({
+            order: [ [ 'available_start_time', 'ASC'] ],
+            where: {
+                name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%`}
+            }
+        })
         res.status(200).json(foundBands)
     } catch (error) {
         res.status(500).json(error)
@@ -69,8 +75,6 @@ bands.delete('/:id', async (req, res) => {
         res.status(500).json(err)
     }
 })
-
-
 
 
 // EXPORT
